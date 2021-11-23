49717325c83b16576f4c4e6c07d154820a550b7a
EvanWard29 <Evan29Ward@gmail.com>
2021-11-23 15:57:59 +0000

    Sessions
    Looked at how sessions can be used using Laravel. Also looked at 'flash' sessions and the difference between them and ordinary sessions.

--- middleware/app/Http/Controllers/HomeController.php
+++ middleware/app/Http/Controllers/HomeController.php
@@ -21,8 +21,26 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        //$request->session()->put(['Evan'=>'student']); //Local session
+        //session(['Edwin'=>'teacher']); //Global session
+
+        //echo $request->session()->get('Evan'); //Return specific session using defined key
+
+        //$request->session()->forget('Edwin'); //Remove specific session
+        //$request->session()->flush(); //Remove all sessions
+
+        //return $request->session()->all(); //Return all sessions stored
+
+        //$request->session()->flash('message', 'Post has been created'); //Lasts only a couple of requests until removed
+        
+        //$request->session()->reflash(); //Keeps flashed sessions a little longer
+
+        //$request->session()->keep('message'); //Saves flashed sessions
+
+        return $request->session()->get('message');
+
+        //return view('home');
     }
 }
