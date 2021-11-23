25f41abde2a148419af69f8863d9915e3a99f637
EvanWard29 <Evan29Ward@gmail.com>
2021-11-23 17:30:30 +0000

    Email
    Looked at how emails can be sent from Laravel using the 'mailgun' API.

---
+++ mail/config/services.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => 'sandbox4df493614ab64944ad3eae4c91cf3ac8.mailgun.org',
+        'secret' => '102dae784b173e9bec0c804901d4d1c1-7dcc6512-9182a6fb',
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => 'us-east-1',
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+];
