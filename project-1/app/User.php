e5df653671e3020a507c89afaf84afa7dcabe74d
EvanWard29 <Evan29Ward@gmail.com>
2021-11-25 14:51:55 +0000

    Implemented Photos
    Added a photos table and model. When creating a new user, admins can upload a photo. This photo is uploaded to the server and saved in the database along with its relation to the user.

--- project-1/app/User.php
+++ project-1/app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -29,5 +30,9 @@ class User extends Authenticatable
 
     public function role(){
         return $this->belongsTo('App\Role');
+    }
+
+    public function photo(){
+        return $this->belongsTo('\App\Photo');
     }
 }
